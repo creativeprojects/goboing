c3f78e5fff8b2d4f7920b92a28240016aa7a87ec
Fred <fred@creativeprojects.tech>
2020-10-28 19:41:02 +0000

    update sprite

--- sprite.go
+++ sprite.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"math"
 
@@ -12,37 +13,63 @@ type XType int
 
 // XType
 const (
-	XDefault XType = iota
-	XLeft
+	XLeft XType = iota
 	XCentre
 	XRight
 )
+
+func (x XType) String() string {
+	switch x {
+	case XCentre:
+		return "X center"
+	case XRight:
+		return "X right"
+	default:
+		return "X left"
+	}
+}
 
 // YType represents the type of the Y coordinate (centre, top or bottom)
 type YType int
 
 // YType
 const (
-	YDefault YType = iota
-	YTop
+	YTop YType = iota
 	YCentre
 	YBottom
 )
 
+func (y YType) String() string {
+	switch y {
+	case YCentre:
+		return "Y center"
+	case YBottom:
+		return "Y bottom"
+	default:
+		return "Y top"
+	}
+}
+
+// SequenceFunc is used as a callback to decide which image to draw
+type SequenceFunc func(int) int
+
 // Sprite manages sprite movement and animation
 type Sprite struct {
-	xType     XType
-	yType     YType
-	frame     int
-	x         float64
-	y         float64
-	image     *ebiten.Image
-	animation []*ebiten.Image
-	sequence  []int
-	rate      int
-	loop      bool
-	started   bool
-	op        *ebiten.DrawImageOptions
+	xType        XType
+	yType        YType
+	x            float64
+	y            float64
+	frame        int             // current frame counter (animation mode)
+	width        int             // fixed size only
+	height       int             // fixed size only
+	image        *ebiten.Image   // single image (no animation)
+	animation    []*ebiten.Image // animation images
+	sequence     []int           // animation sequence (in case same images need reused)
+	sequenceFunc SequenceFunc    // use a callback to decide on animation sequence instead
+	rate         int             // change image every n frame per second
+	loop         bool            // animation loop
+	started      bool            // is animation running?
+	op           *ebiten.DrawImageOptions
 }
 
 // NewSprite creates a new Sprite with default coordinate type
@@ -55,9 +82,25 @@ func NewSprite(xType XType, yType YType) *Sprite {
 	}
 }
 
+// String returns a debug string representation of the sprite current state
+func (s *Sprite) String() string {
+	return fmt.Sprintf("x: %.0f, y: %.0f, frame: %d",
+		s.x,
+		s.y,
+		s.frame,
+	)
+}
+
 // SetImage sets sprite image
 func (s *Sprite) SetImage(image *ebiten.Image) *Sprite {
 	s.image = image
+	return s
+}
+
+// SetSize forces width and height of the image. This is used for calculation only (does not resize the images)
+func (s *Sprite) SetSize(width, height int) *Sprite {
+	s.width = width
+	s.height = height
 	return s
 }
 
@@ -67,41 +110,44 @@ func (s *Sprite) Update() {
 		return
 	}
 	s.frame++
-	if s.frame >= s.rate*len(s.animation) {
+	if !s.loop && s.frame >= s.rate*len(s.animation) {
 		// animation is finished
 		s.started = false
+		return
 	}
+	// update current frame
+	frameID := s.getFrameID()
+	s.image = s.animation[frameID]
 }
 
 // Draw the current image, or the animation to the screen. If no image or animation has been set, it does nothing
 func (s *Sprite) Draw(screen *ebiten.Image) {
-	// try animation first
-	if s.started {
-		s.drawAnimation(screen)
-		return
-	}
-	// try single image next
 	if s.image == nil {
 		log.Println("Sprite.Draw: no image to draw")
 		return
 	}
-	s.draw(screen, s.image)
-}
-
-func (s *Sprite) draw(screen, image *ebiten.Image) {
-	width, height := image.Size()
+	width, height := s.image.Size()
 	s.op.GeoM.Reset()
 	s.op.GeoM.Translate(s.xleft(float64(width)), s.ytop(float64(height)))
-	screen.DrawImage(image, s.op)
+	screen.DrawImage(s.image, s.op)
 }
 
-func (s *Sprite) drawAnimation(screen *ebiten.Image) {
-	frameId := int(math.Mod(math.Floor(float64(s.frame/s.rate)), float64(len(s.animation))))
-	image := s.animation[frameId]
-	if image == nil {
-		return
+func (s *Sprite) getFrameID() int {
+	if s.sequenceFunc != nil {
+		return s.sequenceFunc(s.frame)
 	}
-	s.draw(screen, image)
+	current := float64(s.frame / s.rate)
+	if s.sequence == nil || len(s.sequence) == 0 {
+		// no sequence, we just go through all images one by one
+		frameID := int(math.Mod(current, float64(len(s.animation))))
+		return frameID
+	}
+	frameID := int(math.Mod(current, float64(len(s.sequence))))
+	// if frameID is over the size of animation, we pick the last one
+	if frameID >= len(s.animation) {
+		frameID = len(s.animation) - 1
+	}
+	return frameID
 }
 
 // Start (or restart) an animation
@@ -114,12 +160,21 @@ func (s *Sprite) Start() *Sprite {
 	return s
 }
 
+// Stop animation
+func (s *Sprite) Stop() *Sprite {
+	s.started = false
+	return s
+}
+
 // Animation defines a new animation (but does not start it yet)
 func (s *Sprite) Animation(animation []*ebiten.Image, sequence []int, rate int, loop bool) *Sprite {
 	s.animation = animation
 	s.sequence = sequence
 	s.rate = rate
 	s.loop = loop
+	if s.animation != nil && len(s.animation) > 0 {
+		s.image = s.animation[0]
+	}
 	return s
 }
 
@@ -129,7 +184,15 @@ func (s *Sprite) Animate(animation []*ebiten.Image, sequence []int, rate int, lo
 	return s.Start()
 }
 
-// IsFinished returns true when the animation has finished
+// SetSequenceFunc registers a callback to calculate the next image.
+// the value returned should be a valid index in the animation slice passed to the Animation method
+func (s *Sprite) SetSequenceFunc(sequenceFunc SequenceFunc) *Sprite {
+	s.sequenceFunc = sequenceFunc
+	return s
+}
+
+// IsFinished returns true when the *Sprite animation has finished.
+// An animation with loop = true will never finish
 func (s *Sprite) IsFinished() bool {
 	return !s.started
 }
@@ -152,11 +215,17 @@ func (s *Sprite) MoveTo(x, y float64) *Sprite {
 func (s *Sprite) MoveToType(x, y float64, xType XType, yType YType) *Sprite {
 	if s.xType == xType {
 		s.x = x
+	} else {
+		panic(fmt.Sprintf("mixing different types of coordinates is not yet supported: want to set %s but is %s", xType.String(), s.xType.String()))
 	}
 	if s.yType == yType {
 		s.y = y
+	} else if s.yType == YCentre && yType == YBottom {
+		_, height := s.image.Size()
+		s.y = y - (float64(height) / 2)
+	} else {
+		panic(fmt.Sprintf("mixing different types of coordinates is not yet supported: want to set %s but is %s", yType.String(), s.yType.String()))
 	}
-	panic("Unfinished")
 	return s
 }
 
@@ -190,6 +259,12 @@ func (s *Sprite) Y(yType YType) float64 {
 	default:
 		return s.ytop(float64(height))
 	}
+}
+
+// CollidePoint returns true when the coordinates are "touching" the sprite
+func (s *Sprite) CollidePoint(x, y float64) bool {
+	return s.X(XLeft) <= x && x <= s.X(XRight) &&
+		s.Y(YTop) <= y && y <= s.Y(YBottom)
 }
 
 func (s *Sprite) xleft(width float64) float64 {
